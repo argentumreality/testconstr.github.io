32e9f85d34e0fb28ffdc1439ad366afb8673f5e3
argreality <65363004+argreality@users.noreply.github.com>
2020-07-27 02:57:35 +0300

    Update choice.js

--- js/choice.js
+++ js/choice.js
@@ -19,5 +19,5 @@ var isMobile = {
     }
 };
 if(!isMobile.iOS()){
-    document.getElementById('ar-link').style.display = 'none';
+    //document.getElementById('ar-link').style.display = 'none';
 };
